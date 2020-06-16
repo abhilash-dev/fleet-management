6a88405adfaa789a4f3961632ede02c8dfcf787f
Abhilash Sulibela <abhilash1869@gmail.com>
2020-06-16 16:37:24 -0500

    style(module-api): format the whitespace

--- fleet-management/src/test/java/com/abhilash/fleetmanagement/FleetManagementApplicationTests.java
+++ fleet-management/src/test/java/com/abhilash/fleetmanagement/FleetManagementApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FleetManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
