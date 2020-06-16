e809f5943dd507fcb04894e72136c80054ee1bcd
Abhilash Sulibela <abhilash1869@gmail.com>
2020-06-15 20:01:48 -0500

    feat(module-api): add entities

---
+++ fleet-management/src/main/java/com/abhilash/fleetmanagement/model/Tires.java
@@ -0,0 +1,25 @@
+package com.abhilash.fleetmanagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tires {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private int frontLeft;
+    private int frontRight;
+    private int rearLeft;
+    private int rearRight;
+}
