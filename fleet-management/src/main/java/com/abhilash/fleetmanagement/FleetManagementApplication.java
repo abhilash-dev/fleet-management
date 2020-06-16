1528d8595ca89ac7762ebadcd7e827204541099a
Abhilash Sulibela <abhilash1869@gmail.com>
2020-06-15 20:00:16 -0500

    chore(module-api): init api-module

---
+++ fleet-management/src/main/java/com/abhilash/fleetmanagement/FleetManagementApplication.java
@@ -0,0 +1,13 @@
+package com.abhilash.fleetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleetManagementApplication.class, args);
+    }
+
+}
